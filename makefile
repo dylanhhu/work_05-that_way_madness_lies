25ae30d80a8e779f6a957d8f2e40c16dee5d06eb
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2021-09-28 13:53:17 -0400

    added makefile; work up to step 6

---
+++ makefile
@@ -0,0 +1,5 @@
+all: work05.o
+	gcc -o work05 work05.c
+
+work05.o:
+	gcc -c work05.c
--- work05.c
+++ work05.c
@@ -1,16 +1,51 @@
 #include <stdio.h>
 
 int main() {
+	// 1
 	char myChar = 'a';
 	int myInt = 1234;
 	long myLong = 1234567890;
 
-	printf("myChar: %p\n", &myChar);
-	printf("myChar: %lu\n", &myChar);
-	printf("myInt: %p\n", myInt);
-	printf("myInt: %lu\n", myInt);
-	printf("myLong: %p\n", myLong);
-	printf("myLong: %lu\n", myLong);
+	// 2
+	printf("Address of myChar: %p\n", &myChar);
+	printf("Address of myChar: %lu\n", &myChar);
+	printf("Address of myInt: %p\n", &myInt);
+	printf("Address of myInt: %lu\n", &myInt);
+	printf("Address of myLong: %p\n", &myLong);
+	printf("Address of myLong: %lu\n", &myLong);
+
+	printf("\n");
+
+	// 3
+	char *ptrChar = &myChar;
+	int *ptrInt = &myInt;
+	long *ptrLong = &myLong;
+
+	// 4
+	printf("Value of pointer of myChar: %p\n", ptrChar);
+	printf("Value of pointer of myInt: %p\n", ptrInt);
+	printf("Value of pointer of myLong: %p\n", ptrLong);
+
+	printf("\n");
+
+	// 5
+	*ptrChar = 'b';
+	*ptrInt = 4321;
+	*ptrLong = 987654321;
+
+	printf("Value of myChar: %c\n", myChar);
+	printf("Value of myInt: %d\n", myInt);
+	printf("Value of myLong: %ld\n", myLong);
+
+	printf("\n");
+
+	// 6
+	unsigned int myUnsignedInt = 111111;
+	int *ptrUnsignedIntInt = &myUnsignedInt;
+	char *ptrUnsignedIntChar = &myUnsignedInt;
+
+	printf("ptrUnsignedIntInt: %p points to: %d\n", ptrUnsignedIntInt, *ptrUnsignedIntInt);
+	printf("ptrUnsignedIntChar: %p points to: %c\n", ptrUnsignedIntChar, *ptrUnsignedIntChar);  //not working
 
 	return 0;
 }
